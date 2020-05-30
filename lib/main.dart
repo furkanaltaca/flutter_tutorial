554555c49d765ac6c312f52f49812e7258ddb994
furkanaltaca <furkanaltaca@gmail.com>
2020-05-31 02:42:47 +0300

    New features added.

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:flutter_tutorial/models/Student.dart';
 
 void main() => runApp(MyApp());
 
-String appBarText = "Öğrenci Takip Sistemi";
 final myAppBackgroundColor = Colors.blue;
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: myAppBackgroundColor),
       home: HomeScreen(),
@@ -17,23 +17,65 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   List<Student> studentList = [
-    Student.withId(911, "Furkan", "Altaca", 80),
-    Student.withId(911, "Kasım", "Şahin", 85),
+    Student.withId(911, "Furkan", "Altaca", 100),
+    Student.withId(911, "Mehmet Hazar", "Ertürk", 49),
+    Student.withId(911, "Mehmet", "Aksu", 39),
+    Student.withId(911, "Kasım", "Şahin", 50),
   ];
+
+  Student selectedStudent = Student.withId(0, "Hiç Kimse", "", 0);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(appBarText)), body: buildBody());
+    return Scaffold(
+        appBar: AppBar(title: Text("Öğrenci Takip Sistemi")),
+        body: buildBody());
   }
 
   Widget buildBody() {
-    return Center(
-      child: Text(
-        "My First Flutter App",
-        style: TextStyle(color: Colors.black),
-      ),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: ListView.builder(
+              itemCount: studentList.length,
+              itemBuilder: (BuildContext context, int i) {
+                return ListTile(
+                  title: Text(
+                      studentList[i].firstName + " " + studentList[i].lastName),
+                  subtitle: Text(
+                      "Sınavdan aldığı not: ${studentList[i].grade.toString()} [${studentList[i].getStatus}]"),
+                  leading: CircleAvatar(child: Icon(Icons.person_outline)),
+                  trailing: buildStatusIcon(studentList[i].grade),
+                  onTap: () {
+                    setState(() {
+                      selectedStudent=studentList[i];
+                    });
+                  },
+                  onLongPress: () {
+                    print("Uzun basıldı");
+                  },
+                );
+              }),
+        ),
+        Text("Seçili öğe ${selectedStudent.firstName}")
+      ],
     );
+  }
+
+  Widget buildStatusIcon(int grade) {
+    if (grade >= 50) {
+      return Icon(Icons.done);
+    } else if (grade > 40) {
+      return Icon(Icons.album);
+    } else {
+      return Icon(Icons.clear);
+    }
   }
 }
--- lib/models/Student.dart
+++ lib/models/Student.dart
@@ -1,19 +1,30 @@
 class Student {
   int id;
-  String firtName;
+  String firstName;
   String lastName;
   int grade;
   String status;
 
   Student(String firstName, String lastName, int grade) {
-    this.firtName = firstName;
+    this.firstName = firstName;
     this.lastName = lastName;
     this.grade = grade;
   }
-  Student.withId(int id,String firstName, String lastName, int grade) {
-    this.id=id;
-    this.firtName = firstName;
+  Student.withId(int id, String firstName, String lastName, int grade) {
+    this.id = id;
+    this.firstName = firstName;
     this.lastName = lastName;
     this.grade = grade;
+  }
+
+  String get getStatus {
+    if (this.grade >= 50) {
+      this.status = "Geçti";
+    } else if (this.grade > 40) {
+      this.status = "Bütünlemeye kaldı";
+    } else {
+      this.status = "Kaldı";
+    }
+    return this.status;
   }
 }
